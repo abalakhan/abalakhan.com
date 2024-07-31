5981df876dd7281fb1013db859fa927e2f0f3682
abalakhan <anabalasia@gmail.com>
2024-07-31 13:43:55 -0500

    add recaptcha

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import request from 'request';
 
 const app = express();
 const PORT = process.env.PORT || 3030;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
 
 app.use(express.static("public"));
 
@@ -18,10 +23,36 @@ app.get('/config', (req, res) => {
     res.json({
       secureToken: process.env.SECURE_TOKEN,
       senderEmail: process.env.SENDER_EMAIL,
-      receiverEmail: process.env.RECEIVER_EMAIL
+      receiverEmail: process.env.RECEIVER_EMAIL,
     });
-  });
+});
 
+
+app.post('/submit',function(req,res){
+    /* Browse generates the google-recapcha-response key on form submit.
+    if its blank or null means user has not selected,
+    the captcha then blow error loop occurs.*/
+    if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+    return res.json({"responseCode" : 1,"responseDesc" : "Validate captcha first"});
+    }
+     
+    let secretKey = process.env.RECAPTCHA_SECRET_KEY; // Put your secret key here.
+    let verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+    // Google will respond with success or error scenario on url request sent.
+    request(verificationUrl,function(error,response,body) {
+    body = JSON.parse(body);
+    // Success will be true or false depending upon captcha validation.
+    if(body.success !== undefined && !body.success) {
+    return res.json({"responseCode" : 1,"responseDesc" : "Captcha verification has Failed. Try again."});
+    }
+    res.json({"responseCode" : 0,"responseDesc" : "Captcha validated succesfully!"});
+    });
+    });
+     
+    // for handling 404 requests.
+    app.use("*",function(req,res) {
+    res.status(404).send("404");
+    })
 
 //Data parsing
 app.use(express.urlencoded({
